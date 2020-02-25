9d1a7b4e1cc08c375b64f467a52a688fb15e2581
Valcrist-Luiz <luizbibasfilho@gmail.com>
2020-02-25 13:06:10 -0300

    criação da tabela que recebe do spring

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import './App.css';
+
+class App extends Component {
+
+    state = {
+        isLoading: true,
+        Usuarios: []
+    }
+
+    async componentDidMount() {
+        const response = await fetch('/all');
+        const body = await response.json();
+        this.setState({ Usuarios: body, isLoading: false });
+    }
+
+    render() {
+        const { Usuarios, isLoading } = this.state;
+
+
+        if (isLoading)
+            return (<div>Carregando...</div>);
+
+        return (
+
+            <main><h2>Lista de Usuários</h2>
+                <button type="submit">Novo Usuário</button>
+
+                <div>
+                    <table className="tabelaAll">
+                        <tr>
+                            <th>Id</th>
+                            <th>Nome</th>
+                            <th>CPF</th>
+                            <th>Login</th>
+                            <th>Senha</th>
+                            <th>Papel</th>
+                            <th>CEP</th>
+                            <th>Logradouro</th>
+                            <th>Bairro</th>
+                            <th>Cidade</th>
+                            <th>UF</th>
+                            <th>Complemento</th>
+                        </tr>
+
+
+                        {Usuarios.map(user => (
+                            <tr key={user.id}>
+                                <td>{user.id}</td>
+                                <td>{user.nome}</td>
+                                <td>{user.cpf}</td>
+                                <td>{user.login}</td>
+                                <td>{user.senha}</td>
+                                <td>{user.papel}</td>
+                                <td>{user.cep}</td>
+                                <td>{user.logradouro}</td>
+                                <td>{user.bairro}</td>
+                                <td>{user.cidade}</td>
+                                <td>{user.uf}</td>
+                                <td>{user.complemento}</td>
+                                <td> <a href="/atualizar">Editar</a></td>
+                                <td> <a href="/deletar">Apagar</a></td>
+                            </tr>
+
+                        ))}
+                    </table>
+                </div>
+            </main>
+
+
+        );
+
+
+
+    }
+
+
+}
+
+export default App;
