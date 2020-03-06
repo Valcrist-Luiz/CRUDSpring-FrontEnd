4fcb010bf5eac326fdc85d574d0d2767edef41ef
Valcrist-Luiz <luizbibasfilho@gmail.com>
2020-03-06 16:05:04 -0300

    Modificação da tabela de lista de usuários, inicio da implementacao do deletar usuário

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 
-
-
 import './App.css';
 
 
@@ -10,17 +8,35 @@ class App extends Component {
 
     state = {
         isLoading: true,
-        Usuarios: []
+        Usuarios: [],
     }
 
+
+    async remove(id){
+        await fetch('/api/deletar/${id}' ,{
+            method: 'DELETE',
+            headers:{
+                'Accept' : 'application/json',
+                'Content-type' : 'application/json'
+            }
+        })
+        .then(() =>{
+            let updatedUsuarios = [...this.state.Usuarios].filter(i => i.id !== id);
+            this.setState({Usuarios : updatedUsuarios})
+        });
+    }
+
+
+0
     async componentDidMount() {
-        const response = await fetch('/all');
+        const response = await fetch('/api/all');
         const body = await response.json();
         this.setState({ Usuarios: body, isLoading: false });
     }
 
     render() {
         const { Usuarios, isLoading } = this.state;
+
 
         if (isLoading)
             return (<div>Carregando...</div>);
@@ -39,33 +55,63 @@ class App extends Component {
                             <th>Nome</th>
                             <th>CPF</th>
                             <th>Login</th>
-
+                            <th>Emails</th>
                             <th>Papel</th>
+                            <th>Telefones</th>
                             <th>CEP</th>
                             <th>Logradouro</th>
+                            <th>Complemento</th>
                             <th>Bairro</th>
                             <th>Cidade</th>
                             <th>UF</th>
-                            <th>Complemento</th>
+                            
                         </tr>
 
-
                         {Usuarios.map(user => (
+
+
                             <tr key={user.id}>
                                 <td>{user.id}</td>
                                 <td>{user.nome}</td>
                                 <td>{user.cpf}</td>
                                 <td>{user.login}</td>
+                                <td>
+                                    {(user.emails).map(email => (
+                                        email.email + ", "
+                                    ))}
 
+                                </td>
                                 <td>{user.papel}</td>
+
+                                <td>
+                                    <table className="tabelaTelefone">
+                                        <th>Tipo</th>
+                                        <th>Número</th>
+                                        <tr>
+                                            <td className="tipo">
+                                                {(user.telefones).map(telefone => (
+                                                    telefone.tipo+" "
+                                                ))}
+                                            </td>
+
+                                            <td className="numero"> {(user.telefones).map(telefone => (
+                                                telefone.telefone+" "
+                                                ))}
+                                            </td>
+                                        </tr>
+
+                                    </table>
+                                </td>
+
                                 <td>{user.cep}</td>
                                 <td>{user.logradouro}</td>
+                                <td>{user.complemento}</td>
                                 <td>{user.bairro}</td>
                                 <td>{user.cidade}</td>
                                 <td>{user.uf}</td>
-                                <td>{user.complemento}</td>
+                                
                                 <td> <button className="btEditar">Editar</button></td>
-                                <td><button className="btDeletar" >Deletar</button></td>
+                                <td><button className="btDeletar"  onClick={ ()=> this.remove(user.id)} >Deletar</button></td>
                             </tr>
 
                         ))}
